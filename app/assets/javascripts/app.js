9d9376a0fee0c4672682ca7a9d50c105f05cc285
Tim Habermaas <tim.habermaas@gmail.com>
2013-04-28 21:57:08 +0200

    rename other stuff from 'Auftritte' to 'Aufführungen'

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,9 +2,9 @@ var app = angular.module("hairsprayTickets", ["ngResource"]);
 
 app.config(["$routeProvider", "$httpProvider", function($routeProvider, $httpProvider) {
   $routeProvider.
-    when("/auftritte", {templateUrl: "templates/gigs.html", controller: "GigListCtrl"}).
-    when("/auftritte/:gigId/orders", {templateUrl: "templates/order.html", controller: "OrderCtrl", reloadOnSearch: false}).
-    otherwise({redirectTo: "/auftritte"});
+    when("/auffuehrungen", {templateUrl: "templates/gigs.html", controller: "GigListCtrl"}).
+    when("/auffuehrungen/:gigId/orders", {templateUrl: "templates/order.html", controller: "OrderCtrl", reloadOnSearch: false}).
+    otherwise({redirectTo: "/auffuehrungen"});
 
   $httpProvider.defaults.headers.common["X-CSRF-Token"] = $("meta[name=csrf-token]").attr("content");
 }]);
