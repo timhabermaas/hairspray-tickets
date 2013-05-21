44233b923fc549b9b4ae47b558ad2bb917ce11ab
Tim Habermaas <tim.habermaas@gmail.com>
2013-05-21 22:41:52 +0200

    add segment for /:id to orders api

--- app/api/v1/orders.rb
+++ app/api/v1/orders.rb
@@ -33,37 +33,40 @@ class API::V1::Orders < Grape::API
           end
         end
 
-        desc "Delete an order."
-        delete "/:id" do
-          order = gig.orders.find(params["id"])
-          order.destroy
-        end
+        segment "/:id" do
 
-        desc "Update an order."
-        params do
-          requires :name, type: String, desc: "Name"
-          requires :reduced_count, type: Integer, desc: "Amount of reduced tickets"
-          requires :seat_ids, type: Array, desc: "Seats to be reserved"
-        end
-        put "/:id", rabl: "order" do
-          @order = gig.orders.find params["id"]
-          if !@order.update_attributes(declared(params))
-            error!({"error" => @order.errors}, 422)
+          desc "Delete an order."
+          delete "/" do
+            order = gig.orders.find(params["id"])
+            order.destroy
           end
-        end
 
-        desc "Pay an order."
-        post "/:id/pay", rabl: "order" do
-          @order = gig.orders.find(params["id"])
-          @order.pay!
-          status 200
-        end
+          desc "Update an order."
+          params do
+            requires :name, type: String, desc: "Name"
+            requires :reduced_count, type: Integer, desc: "Amount of reduced tickets"
+            requires :seat_ids, type: Array, desc: "Seats to be reserved"
+          end
+          put "/", rabl: "order" do
+            @order = gig.orders.find params["id"]
+            if !@order.update_attributes(declared(params))
+              error!({"error" => @order.errors}, 422)
+            end
+          end
 
-        desc "Unpay an order."
-        post "/:id/unpay", rabl: "order" do
-          @order = gig.orders.find(params["id"])
-          @order.unpay!
-          status 200
+          desc "Pay an order."
+          post "/pay", rabl: "order" do
+            @order = gig.orders.find(params["id"])
+            @order.pay!
+            status 200
+          end
+
+          desc "Unpay an order."
+          post "/unpay", rabl: "order" do
+            @order = gig.orders.find(params["id"])
+            @order.unpay!
+            status 200
+          end
         end
       end
     end
