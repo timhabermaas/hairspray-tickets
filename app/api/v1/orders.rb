8b19f3a931e7ffd2cd8dea20da983ff318ee6957
Tim Habermaas <tim.habermaas@gmail.com>
2013-05-22 20:08:32 +0200

    rearrange api code for orders by permissions

--- app/api/v1/orders.rb
+++ app/api/v1/orders.rb
@@ -1,80 +1,74 @@
 class API::V1::Orders < API::Base
   helpers Authorization::Helpers
 
-  resource :gigs do
+  namespace "/gigs/:gig_id" do
 
-    namespace "/:gig_id" do
+    helpers do
+      def gig
+        Gig.find(params["gig_id"])
+      end
+    end
 
-      helpers do
-        def gig
-          Gig.find(params["gig_id"])
+    resource :orders do
+
+      desc "Fetches all orders."
+      get "/", rabl: "orders" do
+        @orders = gig.orders
+      end
+
+      segment do # restricted to user, admin
+
+        before { authorize! :user, :admin }
+
+        desc "Create an order."
+        params do
+          requires :name, type: String, desc: "Name"
+          requires :reduced_count, type: Integer, desc: "Amount of reduced tickets"
+          requires :seat_ids, type: Array, desc: "Seats to be reserved"
+        end
+        post "/", rabl: "order" do
+          @order = gig.orders.build declared(params)
+          if !@order.save
+            error!({"error" => @order.errors}, 422)
+          end
+        end
+
+        desc "Delete an order."
+        delete "/:id" do
+          order = gig.orders.find(params["id"])
+          order.destroy
+        end
+
+        desc "Update an order."
+        params do
+          requires :name, type: String, desc: "Name"
+          requires :reduced_count, type: Integer, desc: "Amount of reduced tickets"
+          requires :seat_ids, type: Array, desc: "Seats to be reserved"
+        end
+        put "/:id", rabl: "order" do
+          @order = gig.orders.find params["id"]
+          if !@order.update_attributes(declared(params))
+            error!({"error" => @order.errors}, 422)
+          end
+        end
+
+        desc "Pay an order."
+        post "/:id/pay", rabl: "order" do
+          @order = gig.orders.find(params["id"])
+          @order.pay!
+          status 200
         end
       end
 
-      resource :orders do
+      segment do # restricted to admin
 
-        desc "Fetches all orders."
-        get "/", rabl: "orders" do
-          @orders = gig.orders
-        end
+        before { authorize! :admin }
 
-        segment do
-
-          before { authorize! :user, :admin }
-
-          desc "Create an order."
-          params do
-            requires :name, type: String, desc: "Name"
-            requires :reduced_count, type: Integer, desc: "Amount of reduced tickets"
-            requires :seat_ids, type: Array, desc: "Seats to be reserved"
-          end
-          post "/", rabl: "order" do
-            @order = gig.orders.build declared(params)
-            if !@order.save
-              error!({"error" => @order.errors}, 422)
-            end
-          end
-
-          segment "/:id" do
-
-            desc "Delete an order."
-            delete "/" do
-              order = gig.orders.find(params["id"])
-              order.destroy
-            end
-
-            desc "Update an order."
-            params do
-              requires :name, type: String, desc: "Name"
-              requires :reduced_count, type: Integer, desc: "Amount of reduced tickets"
-              requires :seat_ids, type: Array, desc: "Seats to be reserved"
-            end
-            put "/", rabl: "order" do
-              @order = gig.orders.find params["id"]
-              if !@order.update_attributes(declared(params))
-                error!({"error" => @order.errors}, 422)
-              end
-            end
-
-            desc "Pay an order."
-            post "/pay", rabl: "order" do
-              @order = gig.orders.find(params["id"])
-              @order.pay!
-              status 200
-            end
-          end
-        end
-
-        segment do
-
-          before { authorize! :admin }
-
-          desc "Unpay an order."
-          post "/:id/unpay", rabl: "order" do
-            @order = gig.orders.find(params["id"])
-            @order.unpay!
-            status 200
-          end
+        desc "Unpay an order."
+        post "/:id/unpay", rabl: "order" do
+          @order = gig.orders.find(params["id"])
+          @order.unpay!
+          status 200
         end
       end
     end
