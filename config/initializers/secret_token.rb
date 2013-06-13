8fd64fc99c800daf4511943645ac2362d686ebdc
Tim Habermaas <tim.habermaas@gmail.com>
2013-06-13 21:06:36 +0200

    use environment variable for secret token in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HairsprayTickets::Application.config.secret_token = '5fbe6ba4b2db4ae7b3c8df6ba517b75e52b6cea030fcd1c5e2536ea775a95c6081a0225ac222cc1f3724e61f1d6b6f657b137c2d93f60a659fffbb524bfbbb3a'
+HairsprayTickets::Application.config.secret_token = if Rails.env.production?
+  ENV["SECRET_TOKEN"]
+else
+  '5fbe6ba4b2db4ae7b3c8df6ba517b75e52b6cea030fcd1c5e2536ea775a95c6081a0225ac222cc1f3724e61f1d6b6f657b137c2d93f60a659fffbb524bfbbb3a'
+end
